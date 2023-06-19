18d684ac1de1d2ee8819f08acda8f73deb486aaf
ACE-47 <haidermohammed66233@gmail.com>
2023-06-19 10:31:51 +0300

    commit

--- minutos/project/urls.py
+++ minutos/project/urls.py
@@ -4,6 +4,9 @@ urlpatterns = [
     path('',views.projects, name='projects'),
     path('<int:project_id>/project-detail/',views.project_detail, name='project-detail'),
     path('<int:project_id>/edit-project/',views.edit_project, name='edit-project'),
+    path('<int:project_id>/<int:task_id>/detail/',views.task_detail, name='task-detail'),
+    path('<int:project_id>/<int:task_id>/edit/',views.edit_task, name='edit-task'),
+
 
 
 ]
--- minutos/project/views.py
+++ minutos/project/views.py
@@ -40,7 +40,7 @@ def project_detail(request, project_id):
         task_title = request.POST.get('title')
 
         if task_title:
-            task = Task.objects.create(team = team, project = project, title = task_title)
+            task = Task.objects.create(team = team, project = project, title = task_title, created_by = request.user)
 
             # task.save()
             return redirect('project-detail', project_id = project.id)
@@ -74,3 +74,45 @@ def edit_project(request, project_id):
             return redirect('project-detail', project_id=project.id)
     
     return render(request, 'project/edit_project.html', {'team': team, 'project': project})
+
+
+@login_required
+def task_detail(request, project_id, task_id):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status = Team.ACTIVE)
+    project = get_object_or_404(Project, pk = project_id, team = team)
+    task =get_object_or_404(Task, pk = task_id, team = team)
+
+    
+    return render(request,'project/task_detail.html',{
+        'task':task,
+        'team': team,
+        'project':project
+    })
+
+  
+
+@login_required
+def edit_task(request, project_id, task_id):
+    team = get_object_or_404(Team, pk=request.user.userprofile.active_team_id, status = Team.ACTIVE)
+    task =get_object_or_404(Task, pk = task_id, team = team )
+    project = get_object_or_404(Project, pk = project_id, team = team,)
+
+    
+    if request.method == 'POST':
+            task_edit_title = request.POST.get('title')
+            status = request.POST.get('status')
+            
+            if task_edit_title:
+                task.title = task_edit_title
+                task.status = status
+                task.save()
+                
+
+                messages.info(request, 'The changes was saved!')
+                return redirect('task-detail', project_id = project.id, task_id = task.id)
+    
+    return render(request,'project/edit_task.html',{
+        'task':task,
+        'team': team,
+        'project':project
+    })
