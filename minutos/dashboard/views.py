5ff2db1c1b906e40e4bbf2f4ca195ee72f0bbb42
ACE-47 <haidermohammed66233@gmail.com>
2023-06-23 14:45:04 +0300

    init

--- minutos/dashboard/views.py
+++ minutos/dashboard/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+# 
+# from python
+
+
+from datetime import datetime, timedelta, timezone
+from dateutil.relativedelta import relativedelta
+
+# from django
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+from django.shortcuts import render, get_object_or_404, redirect
+
+from project.models import Entry
+from team.models import Team
 
 # Create your views here.
+
+@login_required
+def dashboard(request):
+    return render(request, 'dashboard/dashboard.html')
