7c123480477f955e30adcbd533237c1137dd1950
ACE-47 <haidermohammed66233@gmail.com>
2023-06-23 14:31:02 +0300

    init

--- minutos/dashboard/urls.py
+++ minutos/dashboard/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('projects/', include('project.urls')),
-    path('myaccount/', include('userprofile.urls')),
-    path('myaccount/teams/', include('team.urls')),
+    
 
 
 
