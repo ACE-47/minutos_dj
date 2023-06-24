7b7b9b78bdf6672f52b1f2ecfdeb47b0a8249d3c
ACE-47 <haidermohammed66233@gmail.com>
2023-06-24 12:23:40 +0300

    init

--- minutos/dashboard/utilities.py
+++ minutos/dashboard/utilities.py
@@ -13,7 +13,7 @@ def get_time_for_team_and_month(team,month):
     return sum(entry.minutes for entry in entries)
 
 
-def time_for_user_and_month(team,user,month):
+def get_time_for_user_and_month(team,user,month):
     entries = Entry.objects.filter(team = team, created_by = user,  created_at__year = month.year,created_at__month = month.month, is_tracked = True)
     return sum(entry.minutes for entry in entries)
 
--- minutos/dashboard/views.py
+++ minutos/dashboard/views.py
@@ -16,7 +16,7 @@ from team.models import Team
 
 # from utilities
 
-from .utilities import get_time_for_user_and_date, get_time_for_team_and_month, time_for_user_and_month, get_time_for_user_and_project_and_month, get_time_for_user_and_team_month
+from .utilities import get_time_for_user_and_date, get_time_for_team_and_month, get_time_for_user_and_month, get_time_for_user_and_project_and_month, get_time_for_user_and_team_month
 # Create your views here.
 
 @login_required
@@ -28,20 +28,42 @@ def dashboard(request):
     all_projects = team.projects.all()
     members = team.members.all()
 
+    # user date , pagination
     num_days = int(request.GET.get('num_days', 0))
-    print(num_days)
-    date_user = datetime.now() - timedelta(days=num_days)
-    print(date_user)
+    date_user = datetime.now() - timedelta(days=num_days) 
     date_entries = Entry.objects.filter(team = team, created_at__date = date_user, created_by = request.user, is_tracked = True)
+
+    # user month , pagination
+    user_num_months = int(request.GET.get('user_num_months',0))
+    user_month = datetime.now() - relativedelta(month=user_num_months)
+
+    for project in all_projects:
+        project.time_for_user_and_project_and_month = get_time_for_user_and_project_and_month(team, project, request.user, user_month)
+
+
+    # user month , pagination
+    team_num_months = int(request.GET.get('team_num_months',0))
+    team_month = datetime.now() - relativedelta(month=team_num_months)
+
+    for member in members:
+        member.time_for_user_team_and_and_month = get_time_for_user_and_team_month(team,member,team_month)
+
 
     context = {
         'team':team,
         'all_projects': all_projects,
+        'latest_projects':all_projects[0:4],
         'members': members,
-        'num_days': date_entries,
+        'num_days': num_days,
         'date_entries' : date_entries,
         'date_user':date_user,
-        'get_time_for_user_and_date': get_time_for_user_and_date(team, request.user, date_user )
+        'user_num_months':user_num_months,
+        'user_month': user_month,
+        'time_for_user_and_month': get_time_for_user_and_month(team,request.user, user_month),
+        'time_for_user_and_date': get_time_for_user_and_date(team, request.user, date_user ),
+        'team_num_months' : team_num_months,
+        'team_month': team_month,
+        'time_for_team_and_month' : get_time_for_team_and_month(team, team_month),
     }
 
 
